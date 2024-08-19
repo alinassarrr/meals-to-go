bfd643e0c5fa501527f79663a880162cc6c8bf89
Ali Nassar <aliinassar8@gmail.com>
2024-08-19 19:00:42 +0300

    Creating Components And TabNav

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+    <SafeAreaView style={styles.container}>
+      <View style ={styles.search}>
+        <Text>Search</Text>
+      </View>
+      <View style={styles.list}>
+        <Text>List</Text>
+      </View>
+    </SafeAreaView>
+    </>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
   },
+  search:{
+    backgroundColor:'green',
+    height:'7%',
+    width:'100%',
+    justifyContent:'center',
+    padding:15
+  },
+  list:{
+    flex:1,
+    backgroundColor: 'blue',
+    width:'100%',
+    padding:20
+  }
 });
