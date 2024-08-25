c2d30374e36d228ab849ca04ebe38c0735eb572a
Ali Nassar <aliinassar8@gmail.com>
2024-08-25 13:34:43 +0300

    downgrading packages versions

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
 // App.js
 import React from "react";
+import 'react-native-gesture-handler';
+
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "./src/infrastructure/Theme";
 import RestaurantsScreen from "./src/features/restaurants/screens/restaurants.screen";
 import { useFonts, Oswald_400Regular } from "@expo-google-fonts/oswald";
 import { Lato_400Regular } from "@expo-google-fonts/lato";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { restaurantRequest } from "./src/services/restaurants/restaurants.services";
 import { RestaurantContextProvider } from "./src/services/restaurants/restaurants.context";
 import { LocationContextProvider } from "./src/services/location/location.context";
 import { Navigation } from "./src/infrastructure/navigation/app.navigator";
 import { FavoritesContextProvider } from "./src/services/favorites/favorites.context";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+
 
 const Tab = createBottomTabNavigator();
 
